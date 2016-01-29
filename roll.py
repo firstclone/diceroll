1dc53e41f444d1b31f5a03e595d59350ab1089d8
firstclone <first.clone@gmail.com>
2016-01-28 16:54:00 -0800

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,8 +1,10 @@
 import random
 def roll(d,s,b):
-total = 0
-while d > 0:
-    total = total + random.randint(1,s)
-    d = d - 1
-final = total + b
-print final
+    total = 0
+    while d > 0:
+        total = total + random.randint(1,s)
+        d = d - 1
+    final = total + b
+    print final
+    
+roll(1,6,0)
